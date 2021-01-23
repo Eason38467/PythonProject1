a6737a6c1bd3110561c1d5f2b026607571a317f1
Eason <630478562@qq.com>
2021-01-23 13:26:16 +0800

    alien_invasion_day_2_add_bullet

--- alin_invasion.py
+++ alin_invasion.py
@@ -5,6 +5,7 @@ import pygame
 from setting import Setting
 from ship import Ship
 import game_function as gf
+from pygame.sprite import Group
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
@@ -15,16 +16,21 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     # 游戏窗口标题
     pygame.display.set_caption("Alien Invasion")
-
     #创建一个飞船
     ship = Ship(ai_settings,screen)
+    #创建一个子弹的组
+    bullets=Group()
     # 设置背景颜色
     bg_color=(230,230,230)
+
     # 开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
-        gf.check_events(ship)
+        gf.check_events(ai_settings,screen ,ship,bullets)
         ship.update()
-        gf.update_screen(ai_settings,screen,ship)
+        gf.update_bullets(bullets)
+        #下面的这个print用来在终端接口看子弹数量，可以删除
+        print(len(bullets))
+        gf.update_screen(ai_settings,screen,ship,bullets)
 
 run_game()
--- game_function.py
+++ game_function.py
@@ -1,12 +1,23 @@
 import sys
 import pygame
+from bullet import Bullet
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event,ai_settings,screen ,ship,bullets):
     """响应按键"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(ai_settings,screen,ship,bullets)
+
+def fire_bullet(ai_settings,screen,ship,bullets):
+    #创建有一个子弹， 并将其加入到组bullets中
+    #限制屏幕中子弹的数量
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings,screen,ship)
+        bullets.add(new_bullet)
+
 
 def check_keyup_events(event, ship):
     """响应松开"""
@@ -15,25 +26,36 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ship):
-    """响应鼠标事件"""
+def check_events(ai_settings,screen,ship,bullets):
+    """响应按键和鼠标事件"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
             #飞船响应左右箭头事件，向左右移动
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event,ship)
+            check_keydown_events(event,ai_settings,screen ,ship,bullets)
 
         elif event.type == pygame.KEYUP:
             check_keyup_events(event,ship)
 
-
- 
-
-def update_screen(ai_settings, screen,ship):
+def update_screen(ai_settings, screen,ship,bullets):
         # 每次循环时都会重绘屏幕
         screen.fill(ai_settings.bg_color)
+
+        #在飞船和外星人后面重绘子弹
+        for bullet in bullets.sprites():
+            bullet.draw_bullet()
+
         ship.blitme()
         # 让最近绘制的屏幕可见
         pygame.display.flip()
+
+def update_bullets(bullets):
+    """更新子弹的位置，并且删除已经消失的子弹"""
+    #更新子弹位置
+    bullets.update()
+    #删除已经消失的子弹
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
