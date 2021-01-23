3ed512874a3293c7de275052a5c0d4c89d44f032
Eason <630478562@qq.com>
2021-01-23 11:57:41 +0800

    alien_invasion_day_2

--- alin_invasion.py
+++ alin_invasion.py
@@ -4,6 +4,7 @@ import sys
 import pygame
 from setting import Setting
 from ship import Ship
+import game_function as gf
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
@@ -16,19 +17,14 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     #创建一个飞船
-    ship = Ship(screen)
+    ship = Ship(ai_settings,screen)
     # 设置背景颜色
     bg_color=(230,230,230)
     # 开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type==pygame.QUIT:
-                sys.exit()
-        
-        # 每次循环时都会重绘屏幕
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        # 让最近绘制的屏幕可见
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings,screen,ship)
+
 run_game()
--- ship.py
+++ ship.py
@@ -1,9 +1,11 @@
 import pygame
 
 class Ship():
-    def __init__(self,screen):
+    def __init__(self,ai_settings,screen):
         """初始化飞船并设置其初始位置"""
         self.screen = screen
+        self.ai_settings = ai_settings
+
         # 加载飞船图像,rect 是矩阵的缩写， 获取其外接矩阵
         self.image = pygame.image.load("images/ship.bmp")
         self.rect = self.image.get_rect()
@@ -12,6 +14,30 @@ class Ship():
         #将飞船放在屏幕底部中央
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        #在飛船的属性center中存储最小数值
+        self.center = float(self.rect.centerx)
+
+        #飞船移动标识
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """根据飞船的移动标致调整飞船位置"""
+        #if self.moving_right:
+        #    self.center += self.ai_settings.ship_speed_factor
+        #if self.moving_left:
+         #   self.center -= self.ai_settings.ship_speed_factor
+        
+        #更新飞船的 center值而不是rect
+
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        #根据self.center 更新rect 对象
+        self.rect.centerx = self.center
 
     def blitme(self):
         """在指定位置绘制飞船 """
